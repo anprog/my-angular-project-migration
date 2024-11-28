0e49e152c30c07239149d4c96d8cc0333037c068
DIR\antonio.a.genghi <antonio.a.genghi>
2024-11-28 21:31:45 +0100

    ho spostato l'invocazione del componente di angularjs nell'appcomponent di angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -31,7 +31,8 @@ import {RowDetailsModalComponent} from './components/row-details-modal/row-detai
     }
 
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {
   constructor() {}
